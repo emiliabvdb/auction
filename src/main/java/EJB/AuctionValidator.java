b15bf25879afb30fdfa1bef69e345c0c26b05943
Frida Klockmann <fridaklockmann@gmail.com>
2018-10-10 12:19:06 +0200

    Lagt til validatorer for Auction publisering og sletting, og for creaing bids

--- src/main/java/EJB/AuctionValidator.java
+++ src/main/java/EJB/AuctionValidator.java
@@ -1,8 +1,12 @@
 package EJB;
 
+import java.util.Date;
+import java.util.List;
+
 import entity.Auction;
 import entity.Bid;
 
+@Stateless
 public abstract class AuctionValidator<T extends Auction> {
 
     private Auction auction;
@@ -10,6 +14,36 @@ public abstract class AuctionValidator<T extends Auction> {
     public AuctionValidator(T t){
         auction = t;
     }
-
-    public abstract Boolean isValidBid(Bid bid);
+    
+    /*
+     * Returns true if the auction can be published
+     */
+    public boolean validateAuctionPublishing() {
+    	//if it's already published, it can´t be published
+    	if(auction.getPublished()) {
+    		return false;
+    	}
+    	//if the auction has ended, you cant publish it again. 
+    	Date now = new Date();
+        if (now.after(auction.getEndDate())) {
+         	return false;
+        }
+        return true;
+    }
+    
+    /*
+     * Returns true if the auction can be deleted
+     */
+    public boolean validateAuctionDeleting() {
+    	//cannot delete auctions where someone has bidded on the product. 
+    	List<Bid> bidList = auction.getBids();
+    	if (bidList.size() != 0) {
+    		return false;
+    	}
+    	return true;
+    	
+    }
+    
+    
+    
 }
---
+++ src/main/java/EJB/BidValidator.java
@@ -0,0 +1,48 @@
+package EJB;
+
+import entity.Bid;
+import entity.User;
+
+import java.util.Date;
+import java.util.List;
+
+import entity.Auction;
+
+@Stateless
+public abstract class BidValidator {
+	public Boolean isValidBid(Bid bid) {
+		Auction bidAuction = bid.getAuction();
+		
+		//The bid has to be the highest bid
+		List<Bid> otherBids = bidAuction.getBids();
+		for(int i = 0; i < otherBids.size(); i++) {
+			if(otherBids.get(i).getAmount() >= bid.getAmount() ) {
+				return false;
+			}
+		}
+		
+		//The auction has to be published
+		if (bidAuction.getPublished() == false) {
+			return false;
+		}
+		
+		//The auction should not be over
+		Date now = new Date();
+		if(now.after(bidAuction.getEndDate())) {
+			return false;
+		}
+		
+		//The auction should have started
+		if(now.before(bidAuction.getStartDate())) {
+			return false;
+		}
+		
+		//The bidder should not be the owner of the auction
+		User bidder = bid.getOwner();
+		User auctOwner = bidAuction.getOwner();
+		if(bidder.equals(auctOwner)) {
+			return false;
+		}
+		return true;
+	}
+}
