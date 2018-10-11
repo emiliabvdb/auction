713395be131b9e20dbb30706be9999d47aa6747c
regtar <fredrik.h.h@outlook.com>
2018-10-11 14:24:56 +0200

    Added better names for the name of the returned value for WSDL for AuctionWebResources

--- src/main/java/SOAP/AuctionWebResource.java
+++ src/main/java/SOAP/AuctionWebResource.java
@@ -5,6 +5,7 @@ import entity.Auction;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import java.util.List;
 
@@ -15,10 +16,12 @@ public class AuctionWebResource {
     @EJB
     AuctionDAO auctionDAO;
 
+    @WebResult(name = "AuctionById")
     public Auction get(Long auctionId) {
         return auctionDAO.findById(auctionId);
     }
 
+    @WebResult(name = "AllAuctions")
     public List<Auction> getPublished() {
         return auctionDAO.findAllPublished();
     }
