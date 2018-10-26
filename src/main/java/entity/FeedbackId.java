4e8a7f0ef007b1c6745595f93ebe681b8fd44fa3
regtar <fredrik.h.h@outlook.com>
2018-10-26 13:12:52 +0200

    Fixed FeedbackID by making it public and adding equals and hashcode.

--- src/main/java/entity/FeedbackId.java
+++ src/main/java/entity/FeedbackId.java
@@ -6,9 +6,45 @@ import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
-class FeedbackId implements Serializable {
+public class FeedbackId implements Serializable {
 
-    @Basic
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FeedbackId other = (FeedbackId) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Basic
     @Column(name = Feedback.AUCTION_ID, nullable = false)
     private Long id;
 
